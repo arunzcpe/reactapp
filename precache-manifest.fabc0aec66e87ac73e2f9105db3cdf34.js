9f20e35a3b463cf4184660f651352f0a295cbb6a
arun <aarunit@gmail.com>
2019-11-20 12:55:38 +0800

    Updates

---
+++ precache-manifest.fabc0aec66e87ac73e2f9105db3cdf34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd63e04422ccf05fa6b91fdca6d7dfe",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "02b09131af1a4c68b2a1",
+    "url": "/reactapp/static/css/main.22de9252.chunk.css"
+  },
+  {
+    "revision": "a0ba525f2d9f93fa2323",
+    "url": "/reactapp/static/js/2.44fa4a56.chunk.js"
+  },
+  {
+    "revision": "02b09131af1a4c68b2a1",
+    "url": "/reactapp/static/js/main.80a6b66b.chunk.js"
+  },
+  {
+    "revision": "9b3aacf1668466850ba6",
+    "url": "/reactapp/static/js/runtime-main.39fd2513.js"
+  }
+]);
